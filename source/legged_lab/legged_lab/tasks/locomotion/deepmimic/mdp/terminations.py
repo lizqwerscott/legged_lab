f2efb6cf80c868525640123bf809a5ceeae612cc
xiaobaige <zitongbai@outlook.com>
2025-11-18 23:24:35 +0800

    global key body pos reset

--- source/legged_lab/legged_lab/tasks/locomotion/deepmimic/mdp/terminations.py
+++ source/legged_lab/legged_lab/tasks/locomotion/deepmimic/mdp/terminations.py
@@ -57,4 +57,29 @@ def deviation_key_body_pos_b(
     max_dist, _ = torch.max(dist, dim=-1)  # shape: (num_envs,)
     return max_dist > threshold
     
+
+def deviation_key_body_pos_w(
+    env: ManagerBasedAnimationEnv, 
+    threshold: float,
+    animation: str,
+    asset_cfg: SceneEntityCfg = SceneEntityCfg("robot", body_names=MISSING, preserve_order=True),
+):
+    robot: Articulation = env.scene[asset_cfg.name]
+    animation_term: AnimationTerm = env.animation_manager.get_term(animation)
     
+    key_body_pos_w = robot.data.body_pos_w[:, asset_cfg.body_ids, :] - env.scene.env_origins.unsqueeze(1)  # shape: (num_envs, M, 3)
+    
+    ref_key_body_pos_b = animation_term.get_key_body_pos_b()[:, 0, :, :]  # shape: (num_envs, M, 3)
+    num_key_bodies = ref_key_body_pos_b.shape[1]
+    ref_root_pos_w = animation_term.get_root_pos_w()[:, 0, :]  # (num_envs, 3)
+    ref_root_quat = animation_term.get_root_quat()[:, 0, :] # (num_envs, 4)
+    ref_key_body_pos_w = ref_root_pos_w.unsqueeze(1) + math_utils.quat_apply(
+        ref_root_quat.unsqueeze(1).expand(-1, num_key_bodies, -1).contiguous(),
+        ref_key_body_pos_b
+    )
+    
+    dist = torch.norm(ref_key_body_pos_w - key_body_pos_w, dim=-1)  # shape: (num_envs, M)
+    max_dist, _ = torch.max(dist, dim=-1)  # shape: (num_envs,)
+    return max_dist > threshold
+    
+
