8dab9cba430d69daf3ae49b179d98d585f22bf85
lizqwer scott <lizqwerscott@163.com>
2026-01-28 11:19:04 +0800

    feat(amp_env_cfg): Increase friction and restitution ranges for robot physics

--- source/legged_lab/legged_lab/tasks/locomotion/amp/amp_env_cfg.py
+++ source/legged_lab/legged_lab/tasks/locomotion/amp/amp_env_cfg.py
@@ -269,9 +269,9 @@ class EventCfg:
         mode="startup",
         params={
             "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
-            "static_friction_range": (0.3, 1.0),
-            "dynamic_friction_range": (0.3, 1.0),
-            "restitution_range": (0.0, 0.0),
+            "static_friction_range": (0.3, 1.6),
+            "dynamic_friction_range": (0.3, 1.2),
+            "restitution_range": (0.0, 0.5),
             "num_buckets": 64,
         },
     )
