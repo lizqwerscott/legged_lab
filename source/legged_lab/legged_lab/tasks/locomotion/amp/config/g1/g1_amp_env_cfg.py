8c00affcc2bcacfa8e15707f35deac20fa754187
lizqwer scott <lizqwerscott@163.com>
2026-01-28 09:32:59 +0800

    feat(g1_amp_env): increase max forward velocity command range
    The linear velocity command range along the x-axis has been expanded
    from (-0.1, 0.1) to (-0.5, 0.5), allowing the robot to receive
    commands for faster forward and backward movement.

--- source/legged_lab/legged_lab/tasks/locomotion/amp/config/g1/g1_amp_env_cfg.py
+++ source/legged_lab/legged_lab/tasks/locomotion/amp/config/g1/g1_amp_env_cfg.py
@@ -261,7 +261,7 @@ class G1AmpEnvCfg(LocomotionAmpEnvCfg):
         # ------------------------------------------------------
         # Commands
         # ------------------------------------------------------
-        self.commands.base_velocity.ranges.lin_vel_x = (-0.1, 0.1)
+        self.commands.base_velocity.ranges.lin_vel_x = (-0.5, 0.5)
         self.commands.base_velocity.ranges.lin_vel_y = (-0.5, 0.5)
         self.commands.base_velocity.ranges.ang_vel_z = (-1.0, 1.0)
         self.commands.base_velocity.ranges.heading = (-math.pi, math.pi)
